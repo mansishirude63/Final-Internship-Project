0c1090e194720c0e12db0ffb6878c390a168b1b4
Mansi Shirude <mansishirude63@gmail.com>
2026-08-14 18:11:28 +0530

    Serializer is changes

--- Restaurant_Ordering_System/restaurant_ordering_system/accounts/serializers.py
+++ Restaurant_Ordering_System/restaurant_ordering_system/accounts/serializers.py
@@ -14,6 +14,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "email",
+            "password",
             "address",
             "status"
         ]
