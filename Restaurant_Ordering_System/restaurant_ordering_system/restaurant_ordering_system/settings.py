35164c26e19c476dac688fa0d745610dbf22495a
Mansi Shirude <mansishirude63@gmail.com>
2026-08-15 12:59:09 +0530

    Connect database to PostgreSQL

--- Restaurant_Ordering_System/restaurant_ordering_system/restaurant_ordering_system/settings.py
+++ Restaurant_Ordering_System/restaurant_ordering_system/restaurant_ordering_system/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'restaurant_ordering_system.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 
